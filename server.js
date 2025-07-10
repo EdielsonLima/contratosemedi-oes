c6dd56b6895c9a569f1b2600fcbe9a19adf2d0e5
EdielsonLima <131915951+EdielsonLima@users.noreply.github.com>
2025-07-10 08:05:00 -0300

    Fix API timeout errors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const RETRY_CONFIG = {
     maxRetries: 3,
     retryDelay: 2000, // 2 segundos
-    timeout: 15000 // 15 segundos
+    timeout: 30000 // 30 segundos
 };
 
 // Função utilitária para fazer requisições com retry e timeout
